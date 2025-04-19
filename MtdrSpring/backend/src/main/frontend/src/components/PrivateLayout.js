931ef116bcb800e5a19f3cf5caf6d23832de9a37
CarlosIvan14 <A01643070@tec.mx>
2025-04-18 21:24:03 -0600

    Integration to the dashboard V7

--- MtdrSpring/backend/src/main/frontend/src/components/PrivateLayout.js
+++ MtdrSpring/backend/src/main/frontend/src/components/PrivateLayout.js
@@ -10,7 +10,7 @@ function PrivateLayout({ user, onLogout }) {
   }
 
   return (
-    <div className="min-h-screen bg-gray-100 border-black border-solid dark:bg-gradient-to-b from-[#7f1d1d] to-[#161513] text-gray-900 dark:text-gray-100">
+    <div className="min-h-screen  border-black border-solid bg-gradient-to-b from-[#7f1d1d] to-[#161513] text-gray-900 text-gray-100">
       <Navbar user={user} onLogout={onLogout} />
       {/* El contenido de las rutas hijas se renderiza aquí */}
       <Outlet />
