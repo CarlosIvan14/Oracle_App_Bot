b169629da7888c667a5c3b51e767a8a2fd54c7ce
Sign0ret <adolfo.h.signoret@gmail.com>
2025-04-20 15:30:36 -0600

    Reports with graphics for both KPIs

--- MtdrSpring/backend/src/main/frontend/src/routes/Reports.jsx
+++ MtdrSpring/backend/src/main/frontend/src/routes/Reports.jsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { useParams }       from 'react-router-dom';
+import { 
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, // BarChart KPI 1
+  PieChart, Pie, Cell // Pie Chart KPI 2
+} from 'recharts';
+
 function Reports() {
   const { projectId } = useParams(); 
   const [sprints, setSprints] = useState([]);
@@ -47,7 +52,8 @@ function Reports() {
 
     let doneTasksCountEndpoint = '';
     let doneTasksDataEndpoint = '';
-    let realHoursEndpoint = '';
+    // let realHoursEndpoint = '';
+    // let timeLogsEndpoint = '';
 
     const isTeam = selectedMember === 'all';
 
@@ -68,6 +74,10 @@ function Reports() {
       //   ? `http://localhost:8081/api/timelogs/team-sprint/${selectedSprint}/real-hours`
       //   : `http://localhost:8081/api/timelogs/individual-sprint/${selectedSprint}/${selectedMember}/real-hours`;
 
+      // timeLogsEndpoint = isTeam
+      //    ? `http://localhost:8081/api/timelogs/team-sprint/${selectedSprint}/time-logs`
+      //    : `http://localhost:8081/api/timelogs/individual-sprint/${selectedSprint}/${selectedMember}/time-logs`;
+
     } else if (filterType === 'week') {
       if (!selectedDate) {
         alert('Selecciona una semana.');
@@ -85,6 +95,10 @@ function Reports() {
       //   ? `http://localhost:8081/api/timelogs/team-week/${selectedDate}/${projectId}/real-hours`
       //   : `http://localhost:8081/api/timelogs/individual-week/${selectedDate}/${selectedMember}/real-hours`;
 
+      // timeLogsEndpoint = isTeam
+      //   ? `http://localhost:8081/api/timelogs/team-week/${selectedDate}/${projectId}/time-logs`
+      //   : `http://localhost:8081/api/timelogs/individual-week/${selectedDate}/${selectedMember}/time-logs`;
+
     } else if (filterType === 'month') {
       if (!selectedDate) {
         alert('Selecciona un mes.');
@@ -101,33 +115,44 @@ function Reports() {
       // realHoursEndpoint = isTeam
       //   ? `http://localhost:8081/api/timelogs/team-month/${selectedDate}/${projectId}/real-hours`
       //   : `http://localhost:8081/api/timelogs/individual-month/${selectedDate}/${selectedMember}/real-hours`;
+
+      // timeLogsEndpoint = isTeam
+      //   ? `http://localhost:8081/api/timelogs/team-month/${selectedDate}/${projectId}/time-logs`
+      //   : `http://localhost:8081/api/timelogs/individual-month/${selectedDate}/${selectedMember}/time-logs`;
+
     }
 
     try {
       const [
         doneRes, 
         doneDataRes, 
-        // hoursRes
+        // hoursRes,
+        // timeLogsRes,
       ] = await Promise.all([
         fetch(doneTasksCountEndpoint),
         fetch(doneTasksDataEndpoint),
         // fetch(realHoursEndpoint),
+        // fetch(timeLogsEndpoint)
       ]);
 
       const [
         doneCount, 
         doneData, 
-        // realHours
+        // realHours,
+        // timeLogs,
       ] = await Promise.all([
         doneRes.json(),
         doneDataRes.json(),
         // hoursRes.json(),
+        // timeLogsRes.json(),
       ]);
 
       const selectedUser = members.find(m => m.id.toString() === selectedMember.toString());
 
       let estimatedHours = 0;
       let realHours = 0;
+      let storyPoints = 0;
+      const storyPointsFrequency = {};
 
       const processedData = doneData.map(item => {
         const { task, ...rest } = item;
@@ -135,26 +160,77 @@ function Reports() {
 
         estimatedHours = estimatedHours + task.estimatedHours;
         realHours = realHours + task.realHours;
-
+        let sp = task.storyPoints;
+        storyPoints = storyPoints + sp;
+        storyPointsFrequency[sp] = (storyPointsFrequency[sp] || 0) + 1;
         
         return {
           ...rest,
           task: taskData,
         };
       });
+
+      const totalSp = Object.values(storyPointsFrequency).reduce((a, b) => a + b, 0);
+
+      const spFrequencyChartData = Object.entries(storyPointsFrequency).map(([sp, count]) => ({
+        name: `${sp} SP`,
+        value: count,
+        percent: ((count / totalSp) * 100).toFixed(1),
+      }));
+
+      let kpi1 = (estimatedHours / realHours) * 100 || 0;
+      kpi1 = Number(kpi1.toFixed(2));
+
+      let kpi2 = (storyPoints / doneData.length) || 0;
+      kpi2 = Number(kpi2.toFixed(2));
+
+      // const processedTimeLogs = timeLogs.map(item => {
+      //   const { idTimeLogs, startTs, endTs } = item;
+      //   return {
+      //     idTimeLogs, startTs, endTs,
+      //   };
+      // });
+
+      console.log("spFrequency:",spFrequencyChartData)
     
       setReportData({
         user: selectedUser?.name || '—',
         tasksDone: doneCount ?? '—',
         realHours: realHours ?? '—',
         tasksData: processedData ?? [],
+        // timeLogs: processedTimeLogs ?? [],
         estimatedHours,
+        storyPoints,
+        spFrequencyChartData: spFrequencyChartData ?? [],
+        kpi1,
+        kpi2,
       });
     } catch (error) {
       console.error('Error generando el reporte:', error);
     }
   };
 
+  // CHARTS KPI 1 and KPI 2.
+
+  const barChartData = reportData?.tasksData?.map((row) => {
+    return {
+      name: row.task.name,
+      real: row.task.realHours || 0,
+      estimated: row.task.estimatedHours || 0,
+    };
+  });
+
+  const COLORS = [
+    '#ffffff', // Blanco
+    '#d3d3d3', // Gris claro
+    '#a9a9a9', // Gris medio
+    '#808080', // Gris oscuro
+    '#585858', // Gris muy oscuro
+    '#2f2f2f', // Gris casi negro
+    '#1a1a1a', // Gris casi negro
+    '#000000'  // Negro
+  ];
+  
   return (
     <div className="p-6 space-y-6">
       <h1 className="text-3xl font-bold text-white">Reportes</h1>
@@ -233,7 +309,7 @@ function Reports() {
         </button>
       </div>
 
-      <div className="mt-6 grid grid-cols-1 sm:grid-cols-4 gap-6">
+      <div className="mt-6 grid grid-cols-1 sm:grid-cols-5 gap-6">
         <div className="bg-black bg-opacity-30 rounded-2xl p-6 shadow-lg">
           <h2 className="text-sm text-gray-400">Usuario</h2>
           <p className="text-2xl font-semibold text-white mt-1">
@@ -245,6 +321,13 @@ function Reports() {
           <h2 className="text-sm text-gray-400">Tareas Completadas</h2>
           <p className="text-3xl font-bold text-green-400 mt-1">
             {reportData?.tasksDone ?? '—'}
+          </p>
+        </div>
+
+        <div className="bg-black bg-opacity-30 rounded-2xl p-6 shadow-lg">
+          <h2 className="text-sm text-gray-400">Story Points</h2>
+          <p className="text-3xl font-bold text-green-400 mt-1">
+            {reportData?.storyPoints ?? '—'}
           </p>
         </div>
 
@@ -263,6 +346,75 @@ function Reports() {
         </div>
       </div>
 
+      {/* Row 2 - BarChart + KPI 1 */}
+      <div className="mt-10 grid grid-cols-6 gap-6">
+        {/* KPI 1 Time Efficiency Score */}
+        <div className="col-span-1 bg-black bg-opacity-30 rounded-2xl p-6 shadow-lg">
+          <h2 className="text-sm text-gray-400">KPI 1 Time Efficiency Score</h2>
+          <p className="text-3xl font-bold text-blue-400 mt-1">
+            {reportData?.kpi1 ?? '—'}
+          </p>
+        </div>
+
+        {/* BarChart KPI 1 */}
+        {barChartData && barChartData.length > 0 && (
+          <div className="col-span-5 bg-black bg-opacity-30 p-4 rounded-2xl">
+            <h2 className="text-xl font-semibold mb-4 text-white">Comparación de Horas por Tarea</h2>
+            <ResponsiveContainer width="100%" height={300}>
+              <BarChart
+                data={barChartData}
+                margin={{ top: 20, right: 30, left: 0, bottom: 5 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" stroke="#555" />
+                <XAxis stroke="#ccc" />
+                <YAxis stroke="#ccc" />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="estimated" fill="#d3d3d3" name="Horas Estimadas" />
+                <Bar dataKey="real" fill="#808080" name="Horas Reales" />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        )}
+      </div>
+
+      {/* Row 3 - PieChart + KPI 2 */}
+      <div className="mt-10 grid grid-cols-6 gap-6">
+        {/* KPI 2 Tryhard Score */}
+        <div className="col-span-1 bg-black bg-opacity-30 rounded-2xl p-6 shadow-lg">
+          <h2 className="text-sm text-gray-400">KPI 2 Tryhard Score</h2>
+          <p className="text-3xl font-bold text-blue-400 mt-1">
+            {reportData?.kpi2 ?? '—'}
+          </p>
+        </div>
+
+        {/* PieChart KPI 2 */}
+        {reportData?.spFrequencyChartData?.length > 0 && (
+          <div className="col-span-5 bg-black bg-opacity-30 p-4 rounded-2xl flex justify-center items-center">
+            <div className="flex justify-center items-center">
+              <h2 className="text-xl font-semibold mb-4 text-white">Dificultad de las tareas</h2>
+              <PieChart width={400} height={400}>
+                <Pie
+                  data={reportData.spFrequencyChartData}
+                  cx="50%"
+                  cy="50%"
+                  label={({ name, value, percent }) => `${name}: ${value} (${percent}%)`}
+                  outerRadius={150}
+                  fill="#8884d8"
+                  dataKey="value"
+                >
+                  {reportData.spFrequencyChartData.map((_, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                  ))}
+                </Pie>
+                <Tooltip formatter={(value, name, props) => [`${value} tasks`, props.payload.name]} />
+                <Legend />
+              </PieChart>
+            </div>
+          </div>
+        )}
+      </div>
+
       {/* TASKS TABLE with rows: member, task, storypoints, status, realHours, estimatedHours, kpi score */}
       {reportData?.tasksData?.length > 0 && (
         <div className="mt-10">
@@ -277,7 +429,7 @@ function Reports() {
                   <th className="px-6 py-3">Estado</th>
                   <th className="px-6 py-3">Horas Reales</th>
                   <th className="px-6 py-3">Horas Estimadas</th>
-                  <th className="px-6 py-3">KPI Score (%)</th>
+                  <th className="px-6 py-3">KPI 1 Time Efficiency Score (%)</th>
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-200">
