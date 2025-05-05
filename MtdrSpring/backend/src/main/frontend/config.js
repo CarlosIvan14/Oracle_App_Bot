5348d4546693bef33449e3afecc46b3cb0a7cc22
ArturoRM22 <A01643269@tec.mx>
2025-05-04 19:22:20 -0600

    apiBase updated

--- MtdrSpring/backend/src/main/frontend/config.js
+++ MtdrSpring/backend/src/main/frontend/config.js
@@ -1,10 +1,10 @@
 
-//const config = {
-//    apiBaseUrl: "http://localhost:8081"
-//  };
-   
 const config = {
-  apiBaseUrl: "http://140.84.170.68"
+  apiBaseUrl: "http://localhost:8081"
 };
+   
+//const config = {
+//  apiBaseUrl: "http://140.84.170.68"
+//};
   
   export default config;
