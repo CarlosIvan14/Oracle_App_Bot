53ff4775cbd67d78b812bb2fd2c19955347ef3a2
ArturoRM22 <A01643269@tec.mx>
2025-06-11 17:50:51 -0600

    Parameterized login e2e with playwright

---
+++ MtdrSpring/backend/src/main/frontend/src/tests/__e2e__/loginParametized.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Login Tests', () => {
+
+  [
+    { name: 'ArturoRM22', password: 'arturo123' },
+    { name: 'AdolfoHS', password: 'adolfo123' },
+    { name: 'CarlosIv14', password: 'carlos123' }
+  ].forEach(({ name, password }) => {
+    
+    test(`@login Login attempt with: ${name}`, async ({ page }) => {
+      await page.goto('http://localhost:8081/');
+      await page.locator('input[type="text"]').click();
+      await page.locator('input[type="text"]').fill(name);
+      await page.locator('input[type="password"]').click();
+      await page.locator('input[type="password"]').fill(password);
+      await page.getByRole('button', { name: 'Ingresar' }).click();
+      await expect(page.getByRole('heading', { name: 'Tus Proyectos' })).toBeVisible();
+      await expect(page.getByRole('button', { name: '👤' })).toBeVisible();
+      await page.getByRole('button', { name: '👤' }).click();
+      await expect(page.getByRole('button', { name: 'Perfil' })).toBeVisible();
+      await page.getByRole('button', { name: 'Perfil' }).click();
+      await expect(page.getByRole('heading', { name: `${name}` })).toBeVisible();
+    });
+  });
+
+  [
+    { name: 'ArturoRM22', password: 'wrongpass' },
+    { name: 'UnknownUser', password: '123456' },
+    { name: 'Attacker', password: 'ggs' }
+  ].forEach(({ name, password }) => {
+    
+    test(`@login Login attempt invalid credentials: ${name}`, async ({ page }) => {
+      await page.goto('http://localhost:8081/');
+      await page.locator('input[type="text"]').click();
+      await page.locator('input[type="text"]').fill(name);
+      await page.locator('input[type="password"]').click();
+      await page.locator('input[type="password"]').fill(password);
+      await page.getByRole('button', { name: 'Ingresar' }).click();
+
+      await expect(page.getByText('Usuario/contraseña inválidos')).toBeVisible();
+    });
+  });
+
+});
